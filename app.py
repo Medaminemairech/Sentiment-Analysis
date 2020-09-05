6826960c93aa1ce49cb00ac3428545fb8ae32fd1
Mohamed amine Mairech <aminemairech0@gmail.com>
2020-09-05 16:18:14 +0100

    dev_fix

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import flask
 import pickle
 import emoji
+import nltk  
+nltk.download('stopwords')
+from nltk.corpus import stopwords          # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer                                 # Python library for NLP
 from tensorflow import keras
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -16,12 +20,20 @@ def main():
     if flask.request.method == 'GET':
         return(flask.render_template('main.html'))
     if flask.request.method == 'POST':
-        
+        stemmer = PorterStemmer()
         max_length = 19
         trunc_type='post'
         padding_type='post'
         oov_tok = "<OOV>"
         new_sample = [flask.request.form['text']]
+        def filter_stop_words(train_sentences, stop_words):
+            for i, sentence in enumerate(train_sentences):
+                new_sent = [stemmer.stem(word) for word in sentence.split() if word not in stop_words]
+                train_sentences[i] = ' '.join(new_sent)
+            return train_sentences
+
+        stop_words = set(stopwords.words("english"))
+        new_sample = filter_stop_words(new_sample, stop_words)
         seq = tokenizer.texts_to_sequences(new_sample)
         padded = pad_sequences(seq, maxlen=max_length, padding=padding_type, truncating=trunc_type)
         prediction=model.predict(padded)
